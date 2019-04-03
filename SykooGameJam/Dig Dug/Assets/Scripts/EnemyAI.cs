26cee320ebdb7b6621b25aa19b9e0bf0472d30c1
Daniel F <trtld2@gmail.com>
2019-04-02 18:44:01 -0700

    Work on Chasing
    Worked on Chasing and improved Ghosting

--- SykooGameJam/Dig Dug/Assets/Scripts/EnemyAI.cs
+++ SykooGameJam/Dig Dug/Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class EnemyAI : MonoBehaviour {
 
     /// <summary>
@@ -45,6 +46,19 @@ public class EnemyAI : MonoBehaviour {
     private LevelManager levelManager;
     private PlayerController playerController;
 
+    private Vector2 nullStruct = new Vector2(90000, 90000);//Kind of a work around here to have a default Vector2 value, like a null value.
+
+    private List<Vector2> previousTravelTargets;//We need this to prevent back tracking
+
+    [SerializeField]
+    private Sprite ghostSprite;
+
+    [SerializeField]
+    private Sprite enemySprite;
+
+    private SpriteRenderer spriteRenderer;
+
+
     //Need this cause if we didn't move since we last changed direction, we're going to think we're not making progress so change directions again (causing a loop of direction changes and never moving anywhere)
     //private bool hasMovedSinceLastDirectionChange = true;
 
@@ -56,30 +70,37 @@ public class EnemyAI : MonoBehaviour {
         levelManager = FindObjectOfType<LevelManager>();
         playerController = FindObjectOfType<PlayerController>();
 
+        previousTravelTargets = new List<Vector2>();
+        previousTravelTargets.Add(nullStruct);
+        previousTravelTargets.Add(nullStruct);//to prevent index out of bounds errors before the list has been populated
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }//TODO: 1. Not new random, new direction with walkable positions. DONE 2. Don't let player dig already dug positions. DONE 3. Restrict enemy movement along gridlines.
 
 
 	// Update is called once per frame
 	void Update () {
 
+        if (currentGoal == Goal.Ghost) {
+            spriteRenderer.sprite = ghostSprite;
+        }
+        else {
+            spriteRenderer.sprite = enemySprite;
+        }
+
         if (currentGoal == Goal.Idle) {
 
             //PrintDirection(travelDirection);
 
             List<Vector2> walkablePositions = FindWalkablePositions();
-            Vector2 bestPostion = new Vector2(900000, 900000);//some crazy far away best position so this will never be the best
+            Vector2 bestPosition = FindBestIdleTarget(walkablePositions);
 
             if (walkablePositions.Count > 0) {
 
                 if (!progressingTowardsTarget) {//If we're not already moving towards a target, lets find a new one.
 
-                    foreach (Vector2 pos in walkablePositions) {
-                        if (DistanceToDirection(pos, travelDirection) < DistanceToDirection(bestPostion, travelDirection)) {//found new best walkable position, which is closest to the direction we want to be traveling in
-                            bestPostion = pos;
-                        }
-                    }
-
-                    if (Vector2.Distance(bestPostion, travelDirection) > lastDirectionDistance) {//We're starting to have to move farther away from our desired direction, lets travel a different direction so we don't get stuck in a loop walking farther and closer over and over
+                    if (Vector2.Distance(bestPosition, travelDirection) > lastDirectionDistance) {//We're starting to have to move farther away from our desired direction, lets travel a different direction so we don't get stuck in a loop walking farther and closer over and over
 
                         if (Time.time - idleTime > 10) {//if we've been idling for more than 10 seconds
                             int rand = Random.Range(1, 5);//25% chance to ghost upon hitting a wall
@@ -93,13 +114,13 @@ public class EnemyAI : MonoBehaviour {
 
                         lastDirectionDistance = 0;//We need to clear this value because we're now working with a new direction, which we could be farther away from, and we'll try to go back if so
 
-                        travelDirection = FindNewOptimalDirection();
+                        travelDirection = FindNewIdleDirection();
 
                     }
 
-                    lastDirectionDistance = Vector2.Distance(bestPostion, travelDirection);
+                    lastDirectionDistance = Vector2.Distance(bestPosition, travelDirection);
 
-                    TravelTarget = bestPostion;
+                    TravelTarget = bestPosition;
 
                 }
 
@@ -116,7 +137,6 @@ public class EnemyAI : MonoBehaviour {
         else if (currentGoal == Goal.Ghost) {
 
             MoveTowardsTarget();
-            Debug.Log("Moving towards travelTarget: " + TravelTarget);
 
             if (!progressingTowardsTarget) {
                 Debug.Log("WE MADE IT! EXPERT GHOSTERS. Now lets chase.");
@@ -126,6 +146,17 @@ public class EnemyAI : MonoBehaviour {
 
         }
         else if (currentGoal == Goal.Chase) {
+            if (!progressingTowardsTarget) {
+                TravelTarget = FindBestChaseTarget(FindWalkablePositions());
+                if (TravelTarget == nullStruct || Vector2.Distance(previousTravelTargets[previousTravelTargets.Count-1], playerController.transform.position) > Vector2.Distance(this.transform.position, playerController.transform.position)) {//we hit a dead end or we're starting to move further from the player, lets ghost to the player
+                    TravelTarget = new Vector2(playerController.transform.position.x, playerController.transform.position.y);
+                    currentGoal = Goal.Ghost;
+                    return;
+                }
+            }
+            else {
+                MoveTowardsTarget();
+            }
 
         }
         else if (currentGoal == Goal.Attack) {
@@ -134,9 +165,7 @@ public class EnemyAI : MonoBehaviour {
         else if (currentGoal == Goal.Flee) {
 
         }
-        else {
 
-        }
 	}
 
     private bool HasReachedTarget() {
@@ -157,7 +186,7 @@ public class EnemyAI : MonoBehaviour {
 
     }
 
-    private Vector2 FindNewOptimalDirection() {//find any direction that has walkable positions
+    private Vector2 FindNewIdleDirection() {//find any direction that has walkable positions
         foreach (Vector2 potentialPos in levelManager.dugPositions) {
             if (levelManager.IsConnected(this.transform.position, potentialPos)) {
 
@@ -177,7 +206,6 @@ public class EnemyAI : MonoBehaviour {
                 }
 
                 int randIndex = Random.Range(0, possibleDirections.Count);
-                Debug.Log("RAND INDEX: " + randIndex);
                 if (possibleDirections.Count >= 1) {
                     return possibleDirections[randIndex];//choose one direction randomly to avoid AI behaviour loop
                 }
@@ -186,6 +214,37 @@ public class EnemyAI : MonoBehaviour {
         }
         Debug.LogError("Could not find a walkable direction. This should never happen, so something went wrong. Defaulting to UP");
         return up;
+    }
+
+    private Vector2 FindBestIdleTarget(List<Vector2> walkablePositions) {
+
+        Vector2 bestPostion = nullStruct;//nullStruct will never be best since it's around 100,000
+
+        foreach (Vector2 pos in walkablePositions) {
+            if (DistanceToDirection(pos, travelDirection) < DistanceToDirection(bestPostion, travelDirection)) {//found new best walkable position, which is closest to the direction we want to be traveling in
+                bestPostion = pos;
+            }
+        }
+
+        return bestPostion;
+
+    }
+
+    private Vector2 FindBestChaseTarget(List<Vector2> walkablePositions) {//can return nullStruct if we are, for example, stuck in a dead end tunnel.
+
+        Vector2 bestPostion = nullStruct;//nullStruct will never be best since it's around 100,000
+
+        foreach (Vector2 pos in walkablePositions) {
+            if (DistanceToDirection(pos, playerController.transform.position) < DistanceToDirection(bestPostion, playerController.transform.position) && pos != previousTravelTargets[previousTravelTargets.Count-2]) {//found new best walkable position, which is closest to the direction we want to be traveling in. COunt-2 = not the current but previous past position
+                bestPostion = pos;
+            }
+        }
+
+        Debug.Log(bestPostion + " != " + previousTravelTargets[previousTravelTargets.Count - 2] + " TRUE?");
+
+        previousTravelTargets.Add(bestPostion);//new previous target
+        return bestPostion;
+
     }
 
     private void MoveTowardsTarget() {
